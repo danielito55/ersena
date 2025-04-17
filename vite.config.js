f31b147f4ed4a0dffeb6167836a3e852c55201cf
danielito55 <150825507+Danieltimana55@users.noreply.github.com>
2025-04-17 16:14:42 -0500

    servicio en ngrok

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: '0.0.0.0'
-        }
-    }
+            host: 'localhost'
+        },
+    },
 });
