200ccbae7a7962ac65922611bce920d5212c0bd2
danielito55 <150825507+Danieltimana55@users.noreply.github.com>
2025-04-18 00:33:26 -0500

    mejoras

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Check if the current host differs from the configured APP_URL host
+        $configuredHost = parse_url(config('app.url'), PHP_URL_HOST);
+        $currentHost = Request::getHost();
+        
+        if ($currentHost && $configuredHost && $currentHost !== $configuredHost) {
+            // We're accessing through a different host, update URLs accordingly
+            $url = Request::getScheme() . '://' . $currentHost;
+            
+            // Force HTTPS for assets if the request is HTTPS
+            if (Request::isSecure()) {
+                URL::forceScheme('https');
+            }
+            
+            // Set the URL in the config
+            config(['app.url' => $url]);
+            config(['app.asset_url' => $url]);
+        }
     }
 }
